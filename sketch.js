adde34173f8254c145466685a6f4dcd54414d0ee
gretaallwyn <68838318+gretaallwyn@users.noreply.github.com>
2020-11-27 22:30:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -157,7 +157,7 @@ function keyPressed(){
 }
 
 async function getBGImg(){
-  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
  var responseJSON = await response.json();
 
   var datetime = responseJSON.datetime;
